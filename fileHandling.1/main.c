f90ab3e9e3c0e952e66a431296131409027135a8
kinyuaMike <garisonmike876@gmail.com>
2024-11-02 05:27:38 +0300

    main.c
    file handling assignment

--- fileHandling.1/main.c
+++ fileHandling.1/main.c
@@ -20,15 +20,15 @@ int main()
 //file storing the data being inputed
     if (file == NULL)
     {
-        printf("Error opening file.\n");
+        printf("Could not open file.\n");
         return 1;
     }
 
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter name of student %d: ", i + 1);
+        printf("Enter the name of the student %d: ", i + 1);
         scanf("%s", students[i].name);
-        printf("Enter marks of student %d: ", i + 1);
+        printf("Enter the marks of the student %d: ", i + 1);
         scanf("%d", &students[i].marks);
         fprintf(file, "Name: %s, Marks: %d\n", students[i].name,students[i].marks);
     }
